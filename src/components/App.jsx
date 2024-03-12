48af6e0d1c755e443be51f1923a2a569a83a0f4b
IgorKryvenko56 <117092561+IgorKryvenko56@users.noreply.github.com>
2024-03-12 23:03:51 +0200

    React hw08PhoneBook upgrated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,46 @@
+import { useDispatch } from 'react-redux';
+import { useEffect, lazy } from 'react';
+import { useAuth } from '../hook/useAuth';
+import { refreshUser } from '../redux/auth/operationAuth';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from '../Layout';
+import { RestrictedRoute } from '../RestrictedRoute';
+import { PrivateRoute } from '../PrivateRoute';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 
+const Home = lazy(() => import('../pages/Home/Home'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage/RegisterPage'));
+const LoginPage = lazy(() => import('../pages/LogIn/LoginPage'));
+const ContactsPage = lazy(() => import('../pages/Contacts/ContactsPage'));
 
-function App() {
- 
+export const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
-  return (
-    <>
-    App
-    </>
-  )
-}
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
 
-export default App
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route
+          path="/register"
+          element={<RestrictedRoute redirectTo="/contacts" component={<RegisterPage />} />}
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />}
+        />
+        <Route
+          path="/contacts"
+          element={<PrivateRoute redirectTo="/login" component={<ContactsPage />} />}
+        />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
